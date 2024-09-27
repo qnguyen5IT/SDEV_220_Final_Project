96ef6914f234f5eb87012569cd982a4ce25b9099
Anthony Hernandez <ahernandez219@ivytech.edu>
2024-09-27 17:41:27 -0400

    Got alien to appear on the screen.

--- alien.py
+++ alien.py
@@ -3,9 +3,15 @@ from pygame.locals import *
 
 
 class Alien:
-    def __init__(self, window):
+    def __init__(self, window, Sprite_sheet, x, y, w, h, new_width, new_height):
         # Set up variables to use, also where rects will be created at.
         self.window = window
+        self.Sprite_sheet = pygame.image.load("Assets/Sprite_sheet.png").convert()
+
+        # Pass new_width and new_height to the get_sprite method
+        self.image = self.get_sprite(x, y, w, h, new_width, new_height)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (10,60)
 
 
     def update(self):
@@ -14,6 +20,17 @@ class Alien:
         pass
 
 
+    def get_sprite(self, x, y, w, h, new_width, new_height):
+        alien = pygame.Surface((w, h)).convert()
+        alien.set_colorkey((255, 255, 255)) # Sprite background is white
+        alien.blit(self.Sprite_sheet, (0, 0), (x, y, w, h)) # Copying the alien from the sprite sheet
+
+        # Scale the alien sprite to the new dimensions
+        alien_scaled = pygame.transform.scale(alien, (new_width, new_height))
+        return alien_scaled
+
+
     def draw(self):
         # This is where the sprite gets drawn to the screen.
+        self.window.blit(self.image, self.rect)
         pass
--- main.py
+++ main.py
@@ -49,7 +49,7 @@ pygame.display.set_caption("Space Invaders")
 
 # Create the objects.
 player = Player(window)
-alien = Alien(window)
+alien = Alien(window, 'Assets/Sprite_sheet.png', 947, 49, 981, 673, 50, 75)
 scoreboard = Scoreboard(window)
 health = PlayerHealth(text_font, 3)
 
